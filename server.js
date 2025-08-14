0d737443c91dbb40a7c72d3c70ec56c9f00cc960
ZubairNaeem <zubairnaeem45@gmail.com>
2025-08-14 13:34:00 +0500

    changes

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('*', (req, res) => {
+// Fix for path-to-regexp error in Express 5
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
